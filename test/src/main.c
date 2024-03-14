ee4a689d972041b7922e310fe5cc73c9be3cc84d
wuyueistiancai@163.com <wuyueistiancai@163.com>
2024-03-14 23:25:15 +0800

    3/14

--- test/src/main.c
+++ test/src/main.c
@@ -1,24 +1,62 @@
 #include <stdio.h>
 #include "video_decoder.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-void image_print(Frame video){
-    for(int i=0;i<video.height;i+=6){
-        for(int j=0;j<=video.width;j+=6){
-            int index=(i*video.height+j)*3;
-            int R=(int)video.data[index];
-            int G=(int)video.data[index+1];
-            int B=(int)video.data[index+2];
-            printf("\33[38;2;%d;%d;%dm█\33[0m",R,G,B);
+
+void image_print(Frame cut, int s) { //average pooling
+    for (int i = 0; i < cut.height; i += s) {
+        for (int j = 0; j <= cut.width; j += s) {
+            int index = i * cut.width + j;
+            int R_sum = 0, G_sum = 0, B_sum = 0;
+            for (int x = 0; x < s; x++) {
+                for (int y = 0; y < s; y++) {
+                    R_sum += cut.data[(index + x + y * cut.width) * 3];
+                    G_sum += cut.data[(index + x + y * cut.width) * 3 + 1];
+                    B_sum += cut.data[(index + x + y * cut.width) * 3 + 2];
+                }
+            }
+            int R = R_sum / s ^ 2;
+            int G = G_sum / s ^ 2;
+            int B = B_sum / s ^ 2;
+            printf("\33[38;2;%d;%d;%dm█", R, G, B);
         }
         printf("\n");
     }
-
 }
 
-int main(){
-    int a = decoder_init("./source/dragon.mp4");
-    Frame cut=decoder_get_frame();
-    image_print(cut);
+void image_print_gray(Frame cut, int s) { //average pooling
+    for (int i = 0; i < cut.height; i += s) {
+        for (int j = 0; j <= cut.width; j += s) {
+            int index = i * cut.width + j;
+            int R_sum = 0, G_sum = 0, B_sum = 0;
+            for (int x = 0; x < s; x++) {
+                for (int y = 0; y < s; y++) {
+                    R_sum += cut.data[(index + x + y * cut.width) * 3];
+                    G_sum += cut.data[(index + x + y * cut.width) * 3 + 1];
+                    B_sum += cut.data[(index + x + y * cut.width) * 3 + 2];
+                }
+            }
+            int R = R_sum / s ^ 2;
+            int G = G_sum / s ^ 2;
+            int B = B_sum / s ^ 2;
+            int Gray = (R + G + B) / 3;
+            printf("\33[38;2;%d;%d;%dm█", Gray, Gray, Gray);
+        }
+        printf("\n");
+    }
+}
+
+
+int main() {
+    int a = decoder_init("./source/bad_apple.mp4");
+    Frame cut;
+    for (int i = 1; i < get_total_frames(); i++) {
+        cut = decoder_get_frame();
+        system("clear");
+        image_print(cut, 6);
+        sleep(0.1);
+    }
     return 0;
 }
 

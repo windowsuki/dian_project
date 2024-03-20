a5ec592fbf28d316999b26af01cebf82d4a772c2
wuyueistiancai@163.com <wuyueistiancai@163.com>
2024-03-20 20:48:42 +0800

    3/20

--- test/src/main.c
+++ test/src/main.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+<<<<<<< HEAD
 #include <termios.h>
 #include <fcntl.h>
 #include <getopt.h>
+=======
+>>>>>>> f0a9906 (3/20)
 
 extern char* optarg;
-int old_print[1000000];
 
+<<<<<<< HEAD
 int optindex = 0;
 struct option longOpts[] = {
     { "help", no_argument, NULL, 'h' },
@@ -53,121 +56,96 @@ void image_print_average(Frame cut, int s) { //average pooling
                     G_sum += cut.data[index * 3 + 1];
                     B_sum += cut.data[index * 3 + 2];
                 }
+=======
+void image_print_average(Frame cut,int s){ //average pooling
+    for(int i=0;i<cut.height;i+=s){
+        for(int j=0;j<=cut.width;j+=s){
+            int R_sum=0,G_sum=0,B_sum=0;
+            for(int y=0;y<s;y++){
+                for(int x=0;x<s;x++){
+                    int index=(i+y)*cut.width+j+x;
+                    R_sum+=cut.data[index*3];
+                    G_sum+=cut.data[index*3+1];
+                    B_sum+=cut.data[index*3+2];
+                }                
+>>>>>>> f0a9906 (3/20)
             }
-            int R = R_sum / s ^ 2;
-            int G = G_sum / s ^ 2;
-            int B = B_sum / s ^ 2;
-            printf("\33[38;2;%d;%d;%dm█", R, G, B);
+            int R=R_sum/s^2;
+            int G=G_sum/s^2;
+            int B=B_sum/s^2;
+            printf("\33[38;2;%d;%d;%dm█",R,G,B);
         }
         printf("\n");
     }
 }
 
-void image_print_gray_average(Frame cut, int s) {
-    for (int i = 0; i < cut.height; i += s) {
-        for (int j = 0; j <= cut.width; j += s) {
-            int index = i * cut.width + j;
-            int R_sum = 0, G_sum = 0, B_sum = 0;
-            for (int y = 0; y < s; y++) {
-                for (int x = 0; x < s; x++) {
-                    int index = (i + y) * cut.width + j + x;
-                    R_sum += cut.data[index * 3];
-                    G_sum += cut.data[index * 3 + 1];
-                    B_sum += cut.data[index * 3 + 2];
-                }
+
+void image_print_gray_average(Frame cut,int s){ 
+    for(int i=0;i<cut.height;i+=s){
+        for(int j=0;j<=cut.width;j+=s){
+            int index=i*cut.width+j;
+            int R_sum=0,G_sum=0,B_sum=0;
+            for(int y=0;y<s;y++){
+                for(int x=0;x<s;x++){
+                    int index=(i+y)*cut.width+j+x;
+                    R_sum+=cut.data[index*3];
+                    G_sum+=cut.data[index*3+1];
+                    B_sum+=cut.data[index*3+2];
+                }                
             }
-            int R = R_sum / s ^ 2;
-            int G = G_sum / s ^ 2;
-            int B = B_sum / s ^ 2;
-            int Gray = (R + G + B) / 3;
-            printf("\33[38;2;%d;%d;%dm█", Gray, Gray, Gray);
+            int R=R_sum/s^2;
+            int G=G_sum/s^2;
+            int B=B_sum/s^2;
+            int Gray=(R+G+B)/3;
+            printf("\33[38;2;%d;%d;%dm█",Gray,Gray,Gray);
         }
         printf("\n");
     }
 }
 
-void image_print_max_init(Frame cut, int s) { //max pooling
-    printf("\033[1;1H");
-    for (int i = 0; i < cut.height; i += s) {
-        for (int j = 0; j <= cut.width; j += s) {
-            int R_max = 0, G_max = 0, B_max = 0;
-            int index0 = i * cut.width + j;
-            for (int y = 0; y < s; y++) {
-                for (int x = 0; x < s; x++) {
-                    int index = (i + y) * cut.width + j + x;
-                    R_max = (R_max > cut.data[index * 3]) ? R_max : cut.data[index * 3];
-                    G_max = (G_max > cut.data[index * 3] + 1) ? G_max : cut.data[index * 3 + 1];
-                    B_max = (B_max > cut.data[index * 3] + 1) ? B_max : cut.data[index * 3 + 2];
-                }
+void image_print_max(Frame cut,int s){ //max pooling
+    for(int i=0;i<cut.height;i+=s){
+        for(int j=0;j<=cut.width;j+=s){
+            int R_max=0,G_max=0,B_max=0;
+            for(int y=0;y<s;y++){
+                for(int x=0;x<s;x++){
+                    int index=(i+y)*cut.width+j+x;
+                    R_max=(R_max>cut.data[index*3])?R_max:cut.data[index*3];
+                    G_max=(G_max>cut.data[index*3]+1)?G_max:cut.data[index*3+1];
+                    B_max=(B_max>cut.data[index*3]+1)?B_max:cut.data[index*3+2];
+                }                
             }
-            printf("\033[38;2;%d;%d;%dm█", R_max, G_max, B_max);
-            old_print[index0 * 3] = R_max;
-            old_print[index0 * 3 + 1] = G_max;
-            old_print[index0 * 3 + 2] = B_max;
+            printf("\33[38;2;%d;%d;%dm█",R_max,G_max,B_max);
         }
         printf("\n");
     }
 }
 
-void image_print_max(Frame cut, int s) { //max pooling
-    printf("\033[1;1H");
-    for (int i = 0; i < cut.height; i += s) {
-        for (int j = 0; j < cut.width; j += s) {
-            int R_max = 0, G_max = 0, B_max = 0;
-            int index0 = i * cut.width + j;
-            for (int y = 0; y < s; y++) {
-                for (int x = 0; x < s; x++) {
-                    int index = (i + y) * cut.width + j + x;
-                    R_max = (R_max > cut.data[index * 3]) ? R_max : cut.data[index * 3];
-                    G_max = (G_max > cut.data[index * 3] + 1) ? G_max : cut.data[index * 3 + 1];
-                    B_max = (B_max > cut.data[index * 3] + 1) ? B_max : cut.data[index * 3 + 2];
-                }
+void image_print_max_gray(Frame cut,int s){
+    for(int i=0;i<cut.height;i+=s){
+        for(int j=0;j<=cut.width;j+=s){
+            int R_max=0,G_max=0,B_max=0;
+            for(int y=0;y<s;y++){
+                for(int x=0;x<s;x++){
+                    int index=(i+y)*cut.width+j+x;
+                    R_max=(R_max>cut.data[index*3])?R_max:cut.data[index*3];
+                    G_max=(G_max>cut.data[index*3]+1)?G_max:cut.data[index*3+1];
+                    B_max=(B_max>cut.data[index*3]+1)?B_max:cut.data[index*3+2];
+                }                
             }
-            if (R_max != (old_print[index0 * 3]) & (j != (cut.width - 1)))
-                printf("\033[38;2;%d;%d;%dm█", R_max, G_max, B_max);
-            else
-                printf("\033[1C");
-            old_print[index0 * 3] = R_max;
-            old_print[index0 * 3 + 1] = G_max;
-            old_print[index0 * 3 + 2] = B_max;
+            int Gray=(R_max+G_max+B_max)/3;
+            printf("\33[38;2;%d;%d;%dm█",Gray,Gray,Gray);
         }
         printf("\n");
     }
 }
 
-void image_print_max_gray(Frame cut, int s) {
-    printf("\033[1;1H");
-    for (int i = 0; i < cut.height; i += s) {
-        for (int j = 0; j <= cut.width; j += s) {
-            int index0 = i * cut.width + j;
-            int R_max = 0, G_max = 0, B_max = 0;
-            for (int y = 0; y < s; y++) {
-                for (int x = 0; x < s; x++) {
-                    int index = (i + y) * cut.width + j + x;
-                    R_max = (R_max > cut.data[index * 3]) ? R_max : cut.data[index * 3];
-                    G_max = (G_max > cut.data[index * 3] + 1) ? G_max : cut.data[index * 3 + 1];
-                    B_max = (B_max > cut.data[index * 3] + 1) ? B_max : cut.data[index * 3 + 2];
-                }
-            }
-            int Gray = (R_max + G_max + B_max) / 3;
-            if (R_max != (old_print[index0 * 3]) & (j != (cut.width - 1)))
-                printf("\033[38;2;%d;%d;%dm█", Gray, Gray, Gray);
-            else
-                printf("\033[1C");
-            old_print[index0 * 3] = R_max;
-            old_print[index0 * 3 + 1] = G_max;
-            old_print[index0 * 3 + 2] = B_max;
-        }
-        printf("\n");
-    }
-}
 
-int main(int argc, char* argv[]) {
-    int opt = 0, stride = 10, rgbmode = 0, fastmode = 0, pausemode = 0;
-    int ch = 0;
-    char filename[] = "./source/bad_apple.mp4";
-    while (opt = getopt_long(argc, argv, "hvcr:f:",longOpts,&optindex) != -1) {
-        switch (opt) {
+int main(int argc,char* argv[]){
+    int opt=0,stride=6,rgbmode=0;
+    char filename[]="./source/bad_apple.mp4";
+    while(opt=getopt(argc,argv,"hvcr:")!=-1){
+        switch (opt){
         case 'h':
             printf("this is a help");
             return 0;
@@ -175,13 +153,13 @@ int main(int argc, char* argv[]) {
             printf("dian_project v0.1");
             return 0;
         case 'c':
-            rgbmode = 1;
+            rgbmode=1;
             break;
         case 'r':
-            stride = *optarg;
+            stride=*optarg;
             break;
         case 'f':
-            strcpy(filename, optarg);
+            strcpy(filename,optarg);
             break;
         default:
             break;
@@ -189,6 +167,7 @@ int main(int argc, char* argv[]) {
     }
     int a = decoder_init(filename);
     Frame cut;
+<<<<<<< HEAD
     cut = decoder_get_frame();
     system("clear");
     image_print_max_init(cut, stride);
@@ -209,8 +188,15 @@ int main(int argc, char* argv[]) {
             else image_print_max_gray(cut, stride);
         }
         else i--;
+=======
+    for (int i=1;i<get_total_frames();i++){
+        cut=decoder_get_frame();
+        system("clear");
+        if(rgbmode) image_print_max(cut,stride);
+        else image_print_max_gray(cut,stride);
+        sleep(0.1);
+>>>>>>> f0a9906 (3/20)
     }
-    return 0;
 }
 
 //gcc src/main.c -I include/ -L lib/ -l videodecoder -l avformat -l avcodec -l avutil -l swscale -o main.out
